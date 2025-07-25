1e8ec0c0a049199d2b989dfbb2c36d37d1e80a0d
nknab <adverb_rushed_09@icloud.com>
2025-07-25 17:23:17 +0200

    refactor: migrate display logic from ev3dev2 to vex

--- robotbasics/SampleCode2.py
+++ robotbasics/SampleCode2.py
@@ -1,14 +1,35 @@
-#!/usr/bin/env python3
-from ev3dev2.display import Display
-from time import sleep
+from vex import SECONDS, Brain, Color, FontType, wait
 
-lcd = Display()
-lcd.rectangle(clear_screen=False, x1=0, y1=0, x2=177, y2=80, fill_color='black')
-lcd.text_pixels('Hello, there!', clear_screen=False, x=10, y=10, text_color='white')
-lcd.text_pixels('Welcome!', clear_screen=False, x=10, y=20, text_color='white')
-lcd.text_pixels('Enjoy robotics!', clear_screen=False, x=10, y=30, text_color='white')
-lcd.update()
-sleep(6)
-lcd.text_pixels('Hello again!', clear_screen=True, x=10, y=10, text_color='white')
-lcd.update()
-sleep(6)
+brain = Brain()
+
+
+# Clear screen with black rectangle and display initial text
+brain.screen.clear_screen()
+brain.screen.set_fill_color("#000000")  # black
+brain.screen.draw_rectangle(0, 0, 480, 240)
+
+brain.screen.set_pen_color("#FFFFFF")  # white
+brain.screen.print_at("Hello, there!", x=10, y=20)
+
+brain.screen.set_pen_color(Color.BLUE)
+brain.screen.print_at("Welcome!", x=10, y=40)
+
+brain.screen.set_pen_color(Color.PURPLE)
+brain.screen.print_at("Enjoy robotics!", x=10, y=60)
+wait(6, SECONDS)
+
+brain.screen.clear_screen()
+brain.screen.set_pen_color(Color.WHITE)
+brain.screen.print_at("Hello again!", x=10, y=20)
+wait(6, SECONDS)
+
+brain.screen.clear_screen()
+img_filepath: str = "images/aix.png"
+
+if brain.sdcard.exists(img_filepath):
+    brain.screen.draw_image_from_file(img_filepath, 0, 0)
+    wait(6, SECONDS)
+else:
+    brain.screen.set_font(FontType.MONO20)
+    brain.screen.set_pen_color(Color.RED)
+    brain.screen.print_at("Image not found at: ", img_filepath, x=50, y=100)
